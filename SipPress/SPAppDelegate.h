7b41518737f20a9760b8fdfedcf7040a1fd18e2b
zppro <zpprozppro@gmail.com>
2012-05-24 13:38:52 +0800

    Initial Commit

---
+++ SipPress/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  SipPress
+//
+//  Created by 钟 平 on 12-5-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
